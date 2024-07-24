d2f622f0f59e822405fcefc98531f8ccb9a5cd67
website <1411mody@gmail.com>
2024-07-24 20:29:21 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// start responsive menu 
+// let menu = document.getElementById("menu");
+// let bar = document.getElementById("bar");
+
+// bar.onclick = function () {
+//     menu.style.transform = " translateY(0)"
+// };
+
+let shop = document.getElementById("shop");
+let mega = document.getElementById("mega");
+
+shop.onclick = function () {
+    mega.style.display = "block";
+    mega.style.top = "48px";
+}
